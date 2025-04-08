fed9234ea2f64c627f2de50c3aee6775fadce460
CnBarrier <CnBarrier0921@outlook.com>
2025-04-08 21:06:56 +0800

    MainMenu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
-
 public class MainMenu : MonoBehaviour
 {
-    void Start()
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
+    }
+
+    public void Settings()
     {
         
     }
 
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
+        Debug.Log("Game Quit!");
     }
+
 }
