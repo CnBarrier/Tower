0659e8aed73c04d9ad316fe7b56614ad9a8b0455
CnBarrier <CnBarrier0921@outlook.com>
2025-04-13 13:41:52 +0800

    新增 主菜单游戏进度选择

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -2,14 +2,29 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    public void StartGame()
+    private CanvasGroup canvasGroup;
+    
+    void Start()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    
+    public void ShowMainMenu()
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+    }    
+ 
+    public void HideMainMenu()
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
     }
 
     public void QuitGame()
     {
-        Application.Quit();
-        Debug.Log("Game Quit!");
+        Application.Quit(); 
     }
 }
